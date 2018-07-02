a34ea0d6eaf95b186bef1eb225abc2bd6f71c7d4
Shiranga Indunil <kgsindunil@outlook.com>
2018-07-02 11:51:57 +1000

    set default page

--- VMS_Project_Solution/VMS_Project/App_Start/RouteConfig.cs
+++ VMS_Project_Solution/VMS_Project/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace VMS_Project
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Car", action = "Create", id = UrlParameter.Optional }
             );
         }
     }
